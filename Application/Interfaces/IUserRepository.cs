6122769eda81f46d84f7db11ddf96447247ef096
Pau <pau.bateria@gmail.com>
2026-01-25 12:56:53 +0100

    Crear interface IUserRepository

---
+++ Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task AddUserAsync(User user);
+        Task UpdateAsync(User user);
+        Task<bool> DeleteUserAsync(User user);
+        Task <User?> GetUserByIdAsync (Guid id);
+        Task <User?> GetUserByEmailAsync (string email);  
+    }
+}
