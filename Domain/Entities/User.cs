2bbde8c3278b95a42d318c569c5218ad1fd9e7ef
Pau <pau.bateria@gmail.com>
2026-01-25 12:56:03 +0100

    Crear entidad user

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!; 
+        public string PasswordSalt { get; set; } = null!;
+
+    }
+}
