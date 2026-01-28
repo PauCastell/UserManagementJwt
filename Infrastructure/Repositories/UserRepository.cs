dff252ccafb61f663ee735c498cf8a4e994310fb
Pau <pau.bateria@gmail.com>
2026-01-28 18:52:32 +0100

    Crear userRepository

---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManagerDbContext _context;
+        public UserRepository(UserManagerDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteUserAsync(User user)
+        {
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User?> GetUserByIdAsync(Guid id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
