cafef94aa875a2beb785e66bbc0a3c2dbed0d7c7
Pau <pau.bateria@gmail.com>
2026-01-28 18:52:10 +0100

    Crear dbContext

---
+++ Infrastructure/Data/UserManagerDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class UserManagerDbContext: DbContext
+    {
+        public UserManagerDbContext(DbContextOptions<UserManagerDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets:
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email) // Añadir índice único en el campo Email
+                .IsUnique();
+        }
+    }
+
+}
---
+++ Infrastructure/Data/UserManagerDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    /*
+     * En tiempo de diseño, Entity Framework Core necesita una forma de crear una instancia del DbContext.
+     * Esta clase implementa IDesignTimeDbContextFactory para proporcionar esa funcionalidad.
+     */
+    internal class UserManagerDbContextFactory : IDesignTimeDbContextFactory<UserManagerDbContext>
+    {
+        public UserManagerDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UserManagerDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=UserApiDb;Trusted_Connection=True;");
+
+            return new UserManagerDbContext(optionsBuilder.Options);
+        }
+    }
+}
