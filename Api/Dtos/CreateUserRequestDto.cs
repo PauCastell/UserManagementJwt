5d0a39f136cbe9ca75f458f06a321011047d9594
Pau <pau.bateria@gmail.com>
2026-01-28 18:51:52 +0100

    Crear dtos

---
+++ Api/Dtos/CreateUserRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Dtos
+{
+    public class CreateUserRequestDto
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ Api/Dtos/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Dtos
+{
+    public class LoginRequestDto
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
