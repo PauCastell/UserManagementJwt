fef38bf74e270062d67510cf8b148a750ba26289
Pau <pau.bateria@gmail.com>
2026-01-31 10:24:41 +0100

    Implementar UserService

--- Application/Services/UserService.cs
+++ Application/Services/UserService.cs
@@ -1,10 +1,49 @@
-﻿using System;
+﻿using Application.Dtos;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
+// El servicio crea y orquesta entidades.
 namespace Application.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        private readonly IUserRepository _userRepository;
+        public readonly IPasswordHasher _passwordHasher;
+
+        public UserService(IUserRepository userRepository, IPasswordHasher passwordHasher)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+        }
+
+        public async Task<UserResponseDto> RegisterAsync(CreateUserRequestDto request)
+        {
+            var existingUser = await _userRepository.GetUserByEmailAsync(request.Email);
+
+            if(existingUser != null)
+            {
+                throw new Exception("El mail ya está registrado.");
+            }
+
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Email = request.Email,
+                PasswordHash = _passwordHasher.Hash(request.Password)
+            };
+
+            await _userRepository.AddUserAsync(user);
+
+            return new UserResponseDto
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email
+            };
+        }
     }
 }
