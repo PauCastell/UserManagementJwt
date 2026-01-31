78e171bb14b0539cd5fab792e2fe0272618fb0bb
Pau <pau.bateria@gmail.com>
2026-01-31 10:24:15 +0100

    Eliminar proyecto de muestra

--- Application/Interfaces/IPasswordHasher.cs
+++ Application/Interfaces/IPasswordHasher.cs
@@ -7,5 +7,6 @@ namespace Application.Interfaces
     public interface IPasswordHasher
     {
         string Hash(string password);
+        bool Verify(string password, string hashedPassword);
     }
 }
