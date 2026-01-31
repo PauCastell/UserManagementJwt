aa6e3e93393ecb0001ca2ded0dcfd94f8fd3bb33
Pau <pau.bateria@gmail.com>
2026-01-31 13:40:15 +0100

    Crear UsersController y el endpoint GetUserById

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(Guid id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+
+            if(user == null) return NotFound();
+
+            return Ok(user);
+        }
+    }
+}
