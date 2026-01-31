48c9bd35a394e4bb0c393a1e78aaffa38a7bfd6f
Pau <pau.bateria@gmail.com>
2026-01-31 13:39:51 +0100

    Agregar RegisterUser al AuthController

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Application.Dtos;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController: ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] CreateUserRequestDto request)
+        {
+            try
+            {
+                var newUser = await _userService.RegisterAsync(request);
+
+                return CreatedAtAction(
+                    "GetUserById",
+                    "Users",
+                    new { id = newUser.Id },
+                    newUser);
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}
