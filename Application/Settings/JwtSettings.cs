af9ab53e765a0b531c1663c464f240039bf997ee
Pau <pau.bateria@gmail.com>
2026-03-02 19:25:21 +0100

    Anotaciones de validación para los datos del token

--- Application/Settings/JwtSettings.cs
+++ Application/Settings/JwtSettings.cs
@@ -1,10 +1,17 @@
-﻿namespace Api.Configuration
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Configuration
 {
     public class JwtSettings
     {
+        [Required]
+        [MinLength(32)]
         public string SecretKey { get; set; } = null!;
+        [Required]
         public string Issuer { get; set; } = null!;
+        [Required]
         public string Audience { get; set; } = null!;
-        public int ExpiryMinutes { get; set; }
+        [Range(1,int.MaxValue)]
+        public int ExpiryMinutes { get; set; } = 60;
     }
 }
