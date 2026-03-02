4ca26282473f6a19a7828d1348596214998154f5
Pau <pau.bateria@gmail.com>
2026-03-02 19:25:42 +0100

    LoginResponse dto

---
+++ Application/Dtos/Loginresponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Dtos
+{
+    public class Loginresponse
+    {
+        public string Token { get; set; } = string.Empty;
+        public Guid UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
