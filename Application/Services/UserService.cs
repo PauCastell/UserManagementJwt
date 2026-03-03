669d7274535f43540fdbf5c8a7304c5b0bd9a044
Pau <pau.bateria@gmail.com>
2026-03-03 19:05:14 +0100

    Modificación, añadir excepciones personalizadas

--- Application/Services/UserService.cs
+++ Application/Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Dtos;
+using Application.Exceptions;
 using Application.Interfaces;
 using Domain.Entities;
 using System;
@@ -12,11 +13,13 @@ namespace Application.Services
     {
         private readonly IUserRepository _userRepository;
         private readonly IPasswordHasher _passwordHasher;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
 
-        public UserService(IUserRepository userRepository, IPasswordHasher passwordHasher)
+        public UserService(IUserRepository userRepository, IPasswordHasher passwordHasher, IJwtTokenGenerator jwtTokenGenerator)
         {
             _userRepository = userRepository;
             _passwordHasher = passwordHasher;
+            _jwtTokenGenerator = jwtTokenGenerator;
         }
 
         public async Task<UserResponseDto> RegisterAsync(CreateUserRequestDto request)
@@ -25,7 +28,7 @@ namespace Application.Services
 
             if(existingUser != null)
             {
-                throw new Exception("El mail ya está registrado.");
+                throw new EmailAlreadyExists($"El email {request.Email} ya está registrado.");
             }
 
             var user = new User
@@ -50,7 +53,47 @@ namespace Application.Services
         {
             var user = await _userRepository.GetUserByIdAsync(id);
 
-            if (user == null) return null;
+            if (user == null) throw new UserNotFoundException($"No se encontró el usuario con id: {id}");
+
+            return new UserResponseDto
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email
+            };
+        }
+
+
+        public async Task<Loginresponse> LoginAsync(LoginRequestDto request)
+        {
+            var user = await _userRepository.GetUserByEmailAsync(request.Email);
+            if(user == null || !_passwordHasher.Verify(request.Password, user.PasswordHash))
+            {
+                throw new InvalidCredentialsException();
+            }
+
+            return new Loginresponse
+            {
+                UserId = user.Id,
+                UserName = user.Name,
+                Email = user.Email,
+                Token = _jwtTokenGenerator.GenerateToken(user)
+            };
+        }
+
+
+        public async Task<UserResponseDto> UpdateUserAsync(Guid id ,UpdateUserDto updatedUser)
+        {
+            var user = await _userRepository.GetUserByIdAsync(id);
+            if(user == null) throw new UserNotFoundException($"No se encontró el usuario con id: {id}");
+
+            var existingUser = await _userRepository.GetUserByEmailAsync(updatedUser.Email);
+            if (existingUser != null && existingUser.Id != id) throw new EmailAlreadyExists();
+           
+            user.Name = updatedUser.Name;
+            user.Email = updatedUser.Email;
+
+            await _userRepository.UpdateAsync(user);
 
             return new UserResponseDto
             {
