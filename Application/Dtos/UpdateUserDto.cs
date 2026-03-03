6bbca43e3a042b2fb9ae5843b46d8a73a497c209
Pau <pau.bateria@gmail.com>
2026-03-03 18:32:28 +0100

    Dto para request de update user

---
+++ Application/Dtos/UpdateUserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Application.Dtos
+{
+    public class UpdateUserDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+    }
+}
